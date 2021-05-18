f2a8f9e054445d59e435cbcdae5ff7bafe1479cc
Harinson Arellan <harrinson.arellan@gmail.com>
2021-05-17 22:52:18 -0400

    Pendientes cambios en StaffCard imagen

--- src/Components/Molecules/StaffCard.jsx
+++ src/Components/Molecules/StaffCard.jsx
@@ -1,20 +1,33 @@
-import image from '../../Assets/img/About/Jennifer-Soft.jpg'
-
-const StaffCard = ({ id, name, job, description }) => (
+const StaffCard = ({ id, name, job, description, image }) => (
     <div className="card mb-3 col-md-6">
         <div className="row g-0">
             <div className="col-md-4">
-                <img src={image} alt={name} />
+                <img src={image}alt={name} />
             </div>
             <div className="col-md-8">
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
                     <h6 className="">{job}</h6>
-                    <p className="card-text">{description}</p>                    
+                    <p className="card-text">{description}</p>
+                    <div className="networks-social">
+                        <div className="network">
+                            <a href="#"><i className="fab fa-facebook"></i></a>
+                        </div>
+                        <div className="network">
+                            <a href="#"><i className="fab fa-twitter"></i></a>
+                        </div>
+                        <div className="network">
+                            <a href="#"><i className="fab fa-instagram"></i></a>
+                        </div>
+                        <div className="network">
+                            <a href="#"><i className="fab fa-youtube"></i></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 )
+
 
 export default StaffCard;
--- src/Components/Organisms/StaffGrid.jsx
+++ src/Components/Organisms/StaffGrid.jsx
@@ -2,18 +2,31 @@ import StaffCard from "../Molecules/StaffCard";
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+import JenniferSotf from '../../Assets/img/About/JenniferSoft.jpg'
+import DaisyWalker from '../../Assets/img/About/DaisyWalker.jpg'
+import FlorenceNelson from '../../Assets/img/About/FlorenceNelson.jpg'
+import ValentinaMartin from '../../Assets/img/About/ValentinaMartin.jpg'
+import { element } from "prop-types";
 
 const StaffGrid = () => {
 
     const [staff, setStaff] = useState([])
+    const [image, setImage] = useState([])
 
     useEffect(() => {
         axios.get('https://60860512d14a87001757894c.mockapi.io/restaurant')
             .then(response => {
                 setStaff(response.data[0].staff);
+                imageStaff(response.data[0].staff)
             })
-
-    }, [])
+            
+        }, [])
+        
+        const imageStaff = async (response) =>{
+            let data = await response
+            let nombre = data.map(({name}) => name.replace(" ", ""))
+            console.log(nombre)
+    }
 
     return (
         <div className="row">
